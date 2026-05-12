75037ddeb2b01240eafa1819d553f2b4a4b820eb
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-05-12 09:38:27 +0530

    terraform

---
+++ 60-vpn/variables.tf
@@ -0,0 +1,26 @@
+variable "project_name" {
+  type        = string
+  default     = "expense"
+ 
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+}
+
+variable "common_tags" {
+  default     = {
+
+    Project = "Expense"
+    terraform = true
+    environment = "dev"
+  }
+}
+variable "vpn_tags" {
+  default = {
+        Component = "openvpn"
+    }
+}
+
+
